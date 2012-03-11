f1091efb1eee5b76a5b67c7b75877f037d573b0a
Habib Ullah Bahar <iambahar@gmail.com>
2012-03-11 16:04:21 +0600

    updated example

--- example.php
+++ example.php
@@ -4,14 +4,12 @@
 
 require_once 'bijoy2unicode.php';
 
-$demo = 'Dt 22 †deªæqvix 1969|';
-
-//$demo = utf8_encode($demo);
-
-//Secho $demo.'   ';
+$demo = '$demo = 'Dfq cv‡k av‡bi kx‡l †ewóZ cvwb‡Z fvmgvb RvZxq dzj kvcjv| Zvi gv_vq cvUMv‡Qi ci¯úi mshy³ wZbwU cvZv Ges Dfh cv‡k `ywU K‡i ZviKv|';
 
 $converted = convertBijoyToUnicode($demo);
 
 echo $converted;
+
+// উভয় পাশে ধানের শীষে বেষ্টিত পানিতে ভাসমান জাতীয় ফুল শাপলা। তার মাথায় পাটগাছের পরস্পর সংযুক্ত তিনটি পাতা এবং উভয পাশে দুটি করে তারকা।
 
 ?>
